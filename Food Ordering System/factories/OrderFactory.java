ede0051b247e9094d8a89eac14833ba1cbe07bb1
ayushman12baghel <ayushman12baghel@gmail.com>
2026-09-19 18:24:41 +0530

    Clean up repository for new LLD projects

--- Food Ordering System/factories/OrderFactory.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface OrderFactory {
-    Order createOrder(User user, Cart cart, Restaurant restaurant, List<MenuItem> menuItems,
-            PaymentStrategy paymentStrategy, double totalCost, String orderType);
-}
